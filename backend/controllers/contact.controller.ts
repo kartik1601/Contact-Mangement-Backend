b1961c06291fb983ad429662bd7f3ef6b043fd60
kartik1601 <kartiku904@gmail.com>
2023-11-20 00:55:49 +0530

    Advanced Error handling -02

--- backend/controllers/contact.controller.ts
+++ backend/controllers/contact.controller.ts
@@ -1,10 +1,11 @@
 import express, {Response,Request} from "express";
 import errorHandler from "../middleware/errorHandler";
+import expressAsyncHandler from "express-async-handler";
 
 // description - GET all contacts
 // route - GET /api/contacts
 // access - public
-export const getContacts = (req:Request,res:Response) => {
+export const getContacts = async(req:Request,res:Response) => {
     res.status(200).json({ // response containing json body
         success: true,
         message: "Welcome to your contacts!"
@@ -13,7 +14,7 @@ export const getContacts = (req:Request,res:Response) => {
 // description - GET single contact
 // route - GET /api/contacts/:id
 // access - public
-export const getSingleContact = (req:Request,res:Response) => {
+export const getSingleContact = async(req:Request,res:Response) => {
     res.status(200).json({ // response containing json body
         success: true,
         message: `The following Contact: ${req.params.id}`
@@ -22,21 +23,23 @@ export const getSingleContact = (req:Request,res:Response) => {
 // description - Create a contact
 // route - POST /api/contacts/
 // access - private
-export const createContact = (req:Request,res:Response) => {
+export const createContact = async(req:Request,res:Response) => {
     console.log(req.body);
     // destructuring the request body
     const {name,email,phone} = req.body;
     if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are compulsory!");
     }
     res.status(201).json({ // response containing json body
         success: true,
-        message: `Contact created: ${req.body.name} with mobile: ${req.body.phone}`
+        message: `Contact created: ${req.body.name}`
     });
 };
 // description - Update a contact
 // route - PUT /api/contacts/:id
 // access - private
-export const updateContact = (req:Request,res:Response) => {
+export const updateContact = async(req:Request,res:Response) => {
     res.status(201).json({ // response containing json body
         success: true,
         message: `Update Contact : ${req.params.id}`
@@ -45,7 +48,7 @@ export const updateContact = (req:Request,res:Response) => {
 // description - Delete all contacts
 // route - DELETE /api/contacts
 // access - private
-export const deleteContact = (req:Request,res:Response) => {
+export const deleteContact = async(req:Request,res:Response) => {
     res.status(200).json({ // response containing json body
         success: true,
         message: `Delete Contact: ${req.params.id}`
--- backend/middleware/errorHandler.ts
+++ backend/middleware/errorHandler.ts
@@ -1,14 +1,52 @@
 // custom middle ware to take care of all errors handling with
 // proper json format
 
-import { Request,Response } from "express";
+import { NextFunction, Request,Response } from "express";
+import { constants } from "../constants";
 
-const errorHandler = (err:Error,req:Request,res:Response) => {
+const errorHandler = (err:Error,req:Request,res:Response,next:NextFunction) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
-    res.json({
-        message: err.message,
-        stackTrace: err.stack
-    });
+    switch(statusCode){
+        case constants.VALIDATION_ERROR:
+            res.json({
+                title: "Validation Failed!",
+                message: err.message,
+                stackTrace: err.stack
+            }); break;
+
+        case constants.NOT_FOUND:
+            res.json({
+                title: "Not Found!",
+                message: err.message,
+                stackTrace: err.stack
+            }); break;
+
+        case constants.UNATHOURIZED:
+            res.json({
+                title: "Unauthorized!",
+                message: err.message,
+                stackTrace: err.stack
+            }); break;
+
+        case constants.FORBIDDEN:
+            res.json({
+                title: "Forbidden!",
+                message: err.message,
+                stackTrace: err.stack
+            }); break;
+
+        case constants.SERVER_ERROR:
+            res.json({
+                title: "Server Error!",
+                message: err.message,
+                stackTrace: err.stack
+            }); break;
+
+        default:
+            console.log("No error! All good.");
+            break;
+    }
+    
 };
 
 export default errorHandler;
